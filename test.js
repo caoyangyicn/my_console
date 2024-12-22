344a6fcac27520e784939bf2d8e68414d7240e3e
caoyangyicn <798188057@qq.com>
2024-12-22 14:45:55 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const my_console = require("my_console");
+class a {
+  constructor() {
+    this.a = 1;
+  }
+}
+let b = new a();
+arr = [1, 2, 3, 4, 5];
+my_console.log("Hello World!");
+my_console.log(JSON.stringify(arr));
+my_console.log(JSON.stringify(b));
